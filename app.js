c57689b3096acfc96151aa8112da460c6e6c4fbe
Thales <thales.y@gmail.com>
2018-10-01 23:20:19 -0300

    updating the percentages: UI controller

--- app.js
+++ app.js
@@ -153,7 +153,8 @@ var UIController = (function () {
     incomeLabel: ".budget__income--value",
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
-    container: ".container"
+    container: ".container",
+    expensesPercLabel: ".item__percentage"
   };
 
   return {
@@ -239,6 +240,34 @@ var UIController = (function () {
       }
     },
 
+    displayPercentages: function (percentages) {
+
+      var fields;
+
+      // returns a node list:
+      fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+      var nodeListForEach = function (list, callback) {
+
+        for (var i = 0; i < list.length; i++) {
+
+          callback(list[i], i);
+
+        }
+      };
+
+      nodeListForEach(fields, function (current, index) {
+
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        } else {
+          current.textContent = '---';
+        }
+
+      });
+
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     }
@@ -281,7 +310,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     // 2. Read percentages from the budget controller
     var percentages = budgetCtrl.getPercentages();
     // 3. Update the UI with the new percentages
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
 
   };
 
